d7c80283b6f772b3ba033d8273005ba0500b2a52
vbtechnoproduct <vbtecho002@gmail.com>
2024-06-06 12:28:51 +0000

    CHANGES

--- util/connection.js
+++ util/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  `mongodb://${process?.env?.MONGODB_USERNAME}:${process?.env?.MONGODB_PASSWORD}@206.189.134.152:27017/${process?.env?.MONGODB_DB_NAME}?authSource=admin`,
+  `mongodb+srv://vaghasiyarushit534:m0BPkELSTiMKx4ZH@cluster0.mg8bwob.mongodb.net`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
